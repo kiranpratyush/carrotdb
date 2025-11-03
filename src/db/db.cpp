b443f546175aa30341ae827df94c05d51731e930
Pratyush kiran Rout <pratyush@email.com>
2025-11-03 08:21:59 +0530

    feat: Add support delete multiple items

--- src/db/db.cpp
+++ src/db/db.cpp
@@ -40,7 +40,7 @@ namespace REDIS_NAMESPACE
             handle_llen(c);
 
         else if (is_equal(slice, "LPOP"))
-            handle_lpop(c);
+            handle_lpop(c, total_commands - 1);
 
         else
             handle_ping(c);
@@ -281,10 +281,26 @@ namespace REDIS_NAMESPACE
         c.current_write_position = 0;
     }
 
-    void DB::handle_lpop(ClientContext &c)
+    void DB::handle_lpop(ClientContext &c, int total_commands)
     {
         ParsedToken key_token = Parser::Parse(c);
         std::string key{c.client.read_buffer.data() + key_token.start_pos, key_token.end_pos - key_token.start_pos + 1};
+        std::vector<std::string> result{};
+        uint32_t total_count = 1;
+        total_commands--;
+        if (total_commands > 0)
+        {
+            ParsedToken count_token = Parser::Parse(c);
+            std::string count_token_string{c.client.read_buffer.data() + count_token.start_pos, count_token.end_pos - count_token.start_pos + 1};
+            try
+            {
+                total_count = std::stoll(count_token_string);
+            }
+            catch (...)
+            {
+                total_count = 1;
+            }
+        }
         auto it = store.find(key);
         if (it == store.end())
         {
@@ -299,37 +315,52 @@ namespace REDIS_NAMESPACE
             c.current_write_position = 0;
             return;
         }
-        unsigned char *p = lpFirst(redisObj.listPack.get());
-        if (!p)
+        while (total_count > 0)
+        {
+            unsigned char *p = lpFirst(redisObj.listPack.get());
+            if (!p)
+            {
+                // List is empty, break out
+                break;
+            }
+
+            // Get the value before deleting
+            int64_t value_length;
+            unsigned char *valuePtr = lpGetWithSize(p, &value_length, nullptr);
+
+            if (valuePtr) // It's a string
+            {
+                result.push_back(std::string(reinterpret_cast<char *>(valuePtr), value_length));
+            }
+            else // It's an integer
+            {
+                result.push_back(std::to_string(value_length));
+            }
+
+            // Delete the first element (pass p, not valuePtr!)
+            auto listpackPtr = lpDelete(std::move(redisObj.listPack), p, nullptr);
+
+            // Store the modified listpack back
+            redisObj.listPack = std::move(listpackPtr);
+
+            total_count--;
+        }
+        if (result.size() == 0)
         {
             c.client.write_buffer.append("$-1\r\n");
             c.current_write_position = 0;
             return;
         }
-
-        // Get the value before deleting
-        int64_t value_length;
-        unsigned char *valuePtr = lpGetWithSize(p, &value_length, nullptr);
-        std::string result;
-
-        if (valuePtr) // It's a string
+        else
         {
-            result = std::string(reinterpret_cast<char *>(valuePtr), value_length);
+            c.client.write_buffer.append("*" + std::to_string(result.size()) + "\r\n");
+            for (const auto &item : result)
+            {
+                c.client.write_buffer.append("$" + std::to_string(item.length()) + "\r\n");
+                c.client.write_buffer.append(item + "\r\n");
+            }
+            c.current_write_position = 0;
         }
-        else // It's an integer
-        {
-            result = std::to_string(value_length);
-        }
-
-        // Delete the first element (pass p, not valuePtr!)
-        auto listpackPtr = lpDelete(std::move(redisObj.listPack), p, nullptr);
-
-        // Store the modified listpack back
-        redisObj.listPack = std::move(listpackPtr);
-
-        // Build proper RESP bulk string response
-        c.client.write_buffer.append("$" + std::to_string(result.length()) + "\r\n" + result + "\r\n");
-        c.current_write_position = 0;
     }
 
     void DB::handle_get(ClientContext &c)
