a78da08c5f9a8a7c987780e9d45a490c0f8386c7
Pratyush kiran Rout <pratyushkiranrout@gmail.com>
2026-02-27 13:23:23 +0530

    feat: add basic passowrd field

--- src/db/ACL/acl_getuser_db.cpp
+++ src/db/ACL/acl_getuser_db.cpp
@@ -10,14 +10,18 @@ namespace REDIS_NAMESPACE
         std::string username = cmd->username.empty() ? "default" : cmd->username;
         
         std::vector<std::string> user_flags;
+        std::vector<std::string> passwords;
         if (c.client->user)
         {
             user_flags = c.client->user->flags;
+            passwords = c.client->user->passwords;
         }
 
-        encode_array_header(&c.client->write_buffer, 2);
+        encode_array_header(&c.client->write_buffer, 4);
         encode_bulk_string(&c.client->write_buffer, "flags");
         encode_bulk_string_array(&c.client->write_buffer, user_flags);
+        encode_bulk_string(&c.client->write_buffer,"passwords");
+        encode_bulk_string_array(&c.client->write_buffer,passwords);
         c.current_write_position = 0;
     }
 }
