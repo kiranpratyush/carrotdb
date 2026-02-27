3135aa4f895a5f4aab517a618d609e288e1447f6
Pratyush kiran Rout <pratyushkiranrout@gmail.com>
2026-02-27 15:13:55 +0530

    chore: format document

--- src/db/ACL/acl_setuser_db.cpp
+++ src/db/ACL/acl_setuser_db.cpp
@@ -5,18 +5,18 @@
 
 namespace REDIS_NAMESPACE
 {
-    void DB::handle_acl_setuser(ClientContext &c,const ServerConfig *config)
+    void DB::handle_acl_setuser(ClientContext &c, const ServerConfig *config)
     {
         ACLSetUserCommand *cmd = static_cast<ACLSetUserCommand *>(c.command.get());
-        
+
         std::string hashedPassword;
         if (cmd->password.has_value())
         {
             hashedPassword = hash_sha256(cmd->password.value());
         }
-        
-        config->aclManager->setUser(cmd->username,hashedPassword,{});
-    
+
+        config->aclManager->setUser(cmd->username, hashedPassword, {});
+
         encode_simple_string(&c.client->write_buffer, "OK");
         c.current_write_position = 0;
     }
